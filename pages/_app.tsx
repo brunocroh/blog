cce2f39a43d1bf63a68bcce7135095850e58114f
Bruno Pinheiro <bruno.pinheiro@tc.com.br>
2022-07-13 14:21:27 -0300

    feat: add basic meta social preview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,20 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Bruno Pinheiro</title>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap&subset=cyrillic" rel="stylesheet" />
+
+        <meta property="og:url" content="https://brunocroh.com" />
+        <meta property="og:type" content="Hello, I’m Bruno Pinheiro, Fullstack and Blockchain Developer based in Goiânia, Brazil." />
+        <meta property="fb:app_id" content="" />
+        <meta
+          property="og:title"
+        content="Bruno Pinheiro Website and blog"
+        />
+        <meta name="twitter:card" content="Hello, I’m Bruno Pinheiro, Fullstack and Blockchain Developer based in Goiânia, Brazil." />
+        <meta
+          property="og:description"
+          content="Hello, I’m Bruno Pinheiro, Fullstack and Blockchain Developer based in Goiânia, Brazil."
+        />
+        <meta property="og:image" content="https://brunocroh.com/me.jpeg" />
       </Head>
       <Main>
         <Component {...pageProps} />
