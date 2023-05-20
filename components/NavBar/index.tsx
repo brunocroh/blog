1ea4cc2421f72b661f7b02175f721757d0d429fd
Bruno Pinheiro <brunocroh@gmail.com>
2023-05-20 20:37:15 -0300

    fix: size of navbar items

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -22,14 +22,14 @@ const NavBar: NextPage = () => {
     <header className="fixed top-0 z-50 flex justify-between container mx-xl min-w-[500px] p-11">
       <Link href="/">
         <a>
-          <span className="text-[rgba(255,255,255,0.5)] text-sm">
+          <span className="text-[rgba(255,255,255,0.5)] text-xl">
             {"Bruno "}
             <strong className="text-white">Pinheiro</strong>
           </span>
         </a>
       </Link>
       <div>
-        <ul className="flex text-sm gap-[40px] text-white">
+        <ul className="flex text-xl gap-[40px] text-white font-bold">
           {routes.map((route) => (
             <li key={route.text}>
               <a
