b29ab67307823b1056a47908138f3d459698fb37
Bruno Pinheiro <brunocroh@gmail.com>
2023-05-21 22:30:55 -0300

    fix: support to mdx blog posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,11 @@
+import HelloWorld from "./hello.mdx";
+
 const Blog = () => {
   return (
     <>
       <h1 className="text-white">Blog</h1>
       <h2 className="text-white">coming soon</h2>
+      <HelloWorld />
     </>
   );
 };
---
+++ mdx-components.tsx
@@ -0,0 +1,16 @@
+import type { MDXComponents } from "mdx/types";
+
+// This file allows you to provide custom React components
+// to be used in MDX files. You can import and use any
+// React component you want, including components from
+// other libraries.
+
+// This file is required to use MDX in `app` directory.
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    // Allows customizing built-in components, e.g. to add styling.
+    h1: ({ children }) => <h1 className="text-white">{children}</h1>,
+    h2: ({ children }) => <h2 className="text-white">{children}</h2>,
+    ...components,
+  };
+}
