62828e551218a9fc50b24c89ceb7681c2b2afe73
Bruno Pinheiro <brunocroh@gmail.com>
2023-05-22 12:13:47 -0300

    chore: notfound base screen

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,10 @@
 export default function NotFound() {
   return (
-    <>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-    </>
-  )
+    <div className="container mx-auto flex h-full w-full justify-center align-center">
+      <div className="h-52">
+        <h2 className="text-white">Not Found</h2>
+        <p className="text-white">Could not find requested resource</p>
+      </div>
+    </div>
+  );
 }
--- components/ContactBar/index.tsx
+++ components/ContactBar/index.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 const ContactBar: NextPage = () => {
   return (
-    <footer className="z-20 w-fit hidden md:flex">
+    <footer className="z-20 w-fit hidden lg:flex">
       <div className="flex items-end">
         <span className="text-sm text-gray-400 whitespace-nowrap mr-10">
-          E: brunocroh@gmail.com{" "}
+          E: brunocroh@gmail.com
         </span>
       </div>
     </footer>
